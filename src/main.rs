6a82662b91db3c7e11d83ed43aed6104385897f9
Mikail Khan <mikail.khan45@gmail.com>
2020-03-15 02:36:25 -0400

    made expression parser

---
+++ src/main.rs
@@ -0,0 +1,282 @@
+use std::io::{self, BufRead, Write};
+
+use std::convert::TryInto;
+
+use std::iter::Peekable;
+use std::str::Chars;
+
+use std::collections::HashMap;
+
+mod parser;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Variable {
+    Integer(isize),
+    Float(f64),
+    Str(String),
+    Custom(HashMap<String, Variable>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Token {
+    LParen,
+    RParen,
+    Assign,
+    Illegal,
+    EOF,
+    Ident(IdentType),
+    Integer(isize),
+    Float(f64),
+    StringLiteral(String),
+    Name(String),
+    Comparison(ComparisonType),
+    Operator(OperatorType),
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum IdentType {
+    Function,
+    Let,
+    If,
+    Else,
+    Return,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum ComparisonType {
+    GreaterEqual,
+    LessEqual,
+    Greater,
+    Less,
+    Equal,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum OperatorType {
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Exponentiate,
+}
+
+struct Lexer<'a> {
+    input: Peekable<Chars<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Lexer {
+            input: input.chars().peekable(),
+        }
+    }
+
+    fn read_char(&mut self) -> Option<char> {
+        self.input.next()
+    }
+
+    fn peek_is_ident_char(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => is_ident_char(ch),
+            None => false,
+        }
+    }
+
+    fn peek_is_numeric_char(&mut self) -> bool {
+        match self.peek_char() {
+            Some(&ch) => ch.is_numeric() || ch == '.',
+            None => false,
+        }
+    }
+
+    fn read_identifier(&mut self, first: char) -> String {
+        let mut ident = String::new();
+        ident.push(first);
+
+        while self.peek_is_ident_char() {
+            ident.push(self.read_char().unwrap());
+        }
+
+        ident
+    }
+
+    fn read_string_literal(&mut self) -> String {
+        let mut ret_str = String::new();
+
+        let mut ch = self.read_char().unwrap();
+        while ch != '\"' {
+            ret_str.push(ch);
+            ch = self.read_char().unwrap();
+        }
+
+        ret_str
+    }
+
+    fn read_num(&mut self, first: char) -> String {
+        let mut literal = String::new();
+        literal.push(first);
+
+        while self.peek_is_numeric_char() {
+            literal.push(self.read_char().unwrap());
+        }
+
+        literal
+    }
+
+    fn peek_char(&mut self) -> Option<&char> {
+        self.input.peek()
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(&ch) = self.peek_char() {
+            if !ch.is_whitespace() {
+                break;
+            }
+            self.read_char();
+        }
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        match self.read_char() {
+            Some('(') => Token::LParen,
+            Some(')') => Token::RParen,
+            Some('+') => Token::Operator(OperatorType::Plus),
+            Some('-') => Token::Operator(OperatorType::Minus),
+            Some('/') => Token::Operator(OperatorType::Divide),
+            Some('*') => Token::Operator(OperatorType::Multiply),
+            Some('^') => Token::Operator(OperatorType::Exponentiate),
+            Some('>') => {
+                if self.peek_char().unwrap() == &'=' {
+                    self.read_char();
+                    Token::Comparison(ComparisonType::GreaterEqual)
+                } else {
+                    Token::Comparison(ComparisonType::Greater)
+                }
+            },
+            Some('<') => {
+                if self.peek_char().unwrap() == &'=' {
+                    self.read_char();
+                    Token::Comparison(ComparisonType::LessEqual)
+                } else {
+                    Token::Comparison(ComparisonType::Less)
+                }
+            },
+            Some('=') => {
+                if self.peek_char().unwrap() == &'=' {
+                    self.read_char();
+                    Token::Comparison(ComparisonType::Equal)
+                } else {
+                    Token::Assign
+                }
+            }
+            Some('\"') => {
+                Token::StringLiteral(self.read_string_literal())
+            },
+            Some(ch @ _) => {
+                if ch.is_numeric() {
+                    let read_number = self.read_num(ch);
+
+                    if let Ok(int) = read_number.parse::<isize>() {
+                        Token::Integer(int)
+                    } else if let Ok(float) = read_number.parse::<f64>() {
+                        Token::Float(float)
+                    } else {
+                        Token::Illegal
+                    }
+                } else if is_ident_char(ch) {
+                    let ident = self.read_identifier(ch);
+                    match ident.as_str() {
+                        "fn" => Token::Ident(IdentType::Function),
+                        "let" => Token::Ident(IdentType::Let),
+                        "If" => Token::Ident(IdentType::If),
+                        "Else" => Token::Ident(IdentType::Else),
+                        "return" => Token::Ident(IdentType::Return),
+                        _ => Token::Name(ident),
+                    }
+                } else {
+                    Token::Illegal
+                }
+            }
+            _ => Token::EOF,
+        }
+    }
+}
+
+fn is_ident_char(ch: char) -> bool {
+    ch.is_alphanumeric() || ch == '_'
+}
+
+fn main() {
+    let stdin = io::stdin();
+
+    let mut vars: HashMap<String, Variable> = HashMap::new();
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+
+        let mut lexer = Lexer::new(&mut line);
+
+        let mut stack: Vec<Token> = Vec::new();
+        stack.push(Token::LParen);
+
+        use parser::{SubExpression, Expression};
+
+        let mut postfix_expr = Expression::new();
+
+        let mut j = 0;
+
+        loop {
+            let token = lexer.next_token();
+
+            match token {
+                Token::LParen => stack.push(token),
+
+                Token::Integer(_) | Token::Float(_) | Token::Name(_) | Token::StringLiteral(_) => {
+                    match token {
+                        Token::Name(name) => postfix_expr.insert(j, SubExpression::Val(vars.get(&name).unwrap().clone())),
+                        Token::Integer(int) => postfix_expr.insert(j, SubExpression::Val(Variable::Integer(int))),
+                        Token::Float(float) => postfix_expr.insert(j, SubExpression::Val(Variable::Float(float))),
+                        Token::StringLiteral(string) => postfix_expr.insert(j, SubExpression::Val(Variable::Str(string))),
+                        _ => panic!(),
+                    }
+                    j += 1;
+                },
+
+                Token::Operator(token_ty) => {
+                    use parser::operator_precedence;
+                    let mut last_val = stack.pop().unwrap();
+                    while let Token::Operator(stack_ty) = last_val {
+                        if ! operator_precedence(stack_ty) > operator_precedence(token_ty) {
+                            break;
+                        }
+                        postfix_expr.insert(j, SubExpression::Operator(stack_ty));
+                        j += 1;
+                        last_val = stack.pop().unwrap();
+                    }
+                    stack.push(last_val);
+                    stack.push(token);
+                },
+
+                Token::RParen | Token::EOF => {
+                    let mut last_val = stack.pop().unwrap();
+                    while last_val != Token::LParen {
+                        postfix_expr.insert(j, parser::token_to_subexpr(last_val));
+                        j += 1;
+                        last_val = stack.pop().unwrap();
+                    }
+                    break;
+                },
+
+                _ => todo!(),
+            }
+        }
+
+        println!("{:?}", parser::eval_expr(postfix_expr, &vars));
+    }
+}
---
+++ src/parser.rs
@@ -0,0 +1,145 @@
+use crate::{Variable, OperatorType, Token};
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum SubExpression {
+    Val(Variable),
+    Operator(OperatorType),
+    Name(String),
+}
+
+pub type Expression = Vec<SubExpression>;
+
+use std::ops;
+
+pub fn eval_expr(expr: Expression, vars: &HashMap<String, Variable>) -> Variable {
+    let mut stack = Expression::new();
+
+    expr.iter().for_each(|subexpr|{
+        match subexpr {
+            SubExpression::Val(v) => stack.push(SubExpression::Val(v.clone())),
+            SubExpression::Name(string) => stack.push(SubExpression::Val(vars.get(string).unwrap().clone())),
+
+            SubExpression::Operator(ty) => {
+                let val1 = match stack.pop().unwrap(){
+                    SubExpression::Val(v) => v,
+                    _ => panic!("not a variable"),
+                };
+                let val2 = match stack.pop().unwrap(){
+                    SubExpression::Val(v) => v,
+                    _ => panic!("not a variable"),
+                };
+
+                let v = match ty {
+                    OperatorType::Plus => val2 + val1,
+                    OperatorType::Minus => val2 - val1,
+                    OperatorType::Multiply => val2 * val1,
+                    OperatorType::Divide => val2 / val1,
+                    OperatorType::Exponentiate => val2.exp(val1),
+                };
+
+                stack.push(SubExpression::Val(v));
+            }
+        }
+    });
+
+    match stack.pop().unwrap() {
+        SubExpression::Val(v) => v,
+        _ => panic!("expr eval'd to not variable"),
+    }
+}
+
+impl ops::Add for Variable {
+    type Output = Variable;
+
+    fn add(self, rhs: Variable) -> Self::Output {
+        match (self, rhs) {
+            (Variable::Integer(v1), Variable::Integer(v2)) => Variable::Integer(v1 + v2),
+            (Variable::Float(v1), Variable::Integer(v2)) => Variable::Float(v1 + v2 as f64),
+            (Variable::Integer(v1), Variable::Float(v2)) => Variable::Float(v1 as f64 + v2),
+            (Variable::Float(v1), Variable::Float(v2)) => Variable::Float(v1 + v2),
+            (Variable::Str(str1), Variable::Str(str2)) => Variable::Str(format!("{}{}", str1, str2)),
+            _ => panic!("illegal addition"),
+        }
+    }
+}
+
+impl ops::Sub for Variable {
+    type Output = Variable;
+
+    fn sub(self, rhs: Variable) -> Self::Output {
+        match (self, rhs) {
+            (Variable::Integer(v1), Variable::Integer(v2)) => Variable::Integer(v1 - v2),
+            (Variable::Float(v1), Variable::Integer(v2)) => Variable::Float(v1 - v2 as f64),
+            (Variable::Integer(v1), Variable::Float(v2)) => Variable::Float(v1 as f64 - v2),
+            (Variable::Float(v1), Variable::Float(v2)) => Variable::Float(v1 - v2),
+            (Variable::Str(_), Variable::Str(_)) => panic!("cannot subtract strings"),
+            _ => panic!("illegal subtraction"),
+        }
+    }
+}
+
+impl ops::Mul for Variable {
+    type Output = Variable;
+
+    fn mul(self, rhs: Variable) -> Self::Output {
+        match (self, rhs) {
+            (Variable::Integer(v1), Variable::Integer(v2)) => Variable::Integer(v1 * v2),
+            (Variable::Float(v1), Variable::Integer(v2)) => Variable::Float(v1 * v2 as f64),
+            (Variable::Integer(v1), Variable::Float(v2)) => Variable::Float(v1 as f64 * v2),
+            (Variable::Float(v1), Variable::Float(v2)) => Variable::Float(v1 * v2),
+            (Variable::Str(_), Variable::Str(_)) => panic!("cannot multiply strings"),
+            _ => panic!("illegal multiplication"),
+        }
+    }
+}
+
+impl ops::Div for Variable {
+    type Output = Variable;
+
+    fn div(self, rhs: Variable) -> Self::Output {
+        match (self, rhs) {
+            (Variable::Integer(v1), Variable::Integer(v2)) => Variable::Integer(v1 / v2),
+            (Variable::Float(v1), Variable::Integer(v2)) => Variable::Float(v1 / v2 as f64),
+            (Variable::Integer(v1), Variable::Float(v2)) => Variable::Float(v1 as f64 / v2),
+            (Variable::Float(v1), Variable::Float(v2)) => Variable::Float(v1 / v2),
+            (Variable::Str(_), Variable::Str(_)) => panic!("cannot multiply strings"),
+            _ => panic!("illegal multiplication"),
+        }
+    }
+}
+
+impl Variable {
+    pub fn exp(self, rhs: Variable) -> Variable {
+        match (self, rhs) {
+            (Variable::Integer(v1), Variable::Integer(v2)) => Variable::Integer(v1.pow(v2 as u32)),
+            (Variable::Float(v1), Variable::Integer(v2)) => Variable::Float(v1.powi(v2 as i32)),
+            (Variable::Integer(v1), Variable::Float(v2)) => Variable::Float((v1 as f64).powf(v2)),
+            (Variable::Float(v1), Variable::Float(v2)) => Variable::Float(v1.powf(v2)),
+            (Variable::Str(_), Variable::Str(_)) => panic!("cannot multiply strings"),
+            _ => panic!("illegal multiplication"),
+        }
+    }
+}
+
+pub fn token_to_subexpr(token: Token) -> SubExpression {
+    match token {
+        Token::Integer(int) => SubExpression::Val(Variable::Integer(int)),
+        Token::Float(float) => SubExpression::Val(Variable::Float(float)),
+        Token::StringLiteral(string) => SubExpression::Val(Variable::Str(string)),
+        Token::Operator(ty) => SubExpression::Operator(ty),
+        Token::Name(name) => SubExpression::Name(name),
+        _ => panic!("{:?} can't be subexpr", token),
+    }
+}
+
+pub fn operator_precedence(ty: OperatorType) -> u8 {
+    match ty {
+        OperatorType::Plus => 1,
+        OperatorType::Minus => 1,
+        OperatorType::Multiply => 2,
+        OperatorType::Divide => 2,
+        OperatorType::Exponentiate => 3,
+    }
+}
